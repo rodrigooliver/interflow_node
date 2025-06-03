dc1356207cf50a0c3621656ec7b058558be311dd
Rodrigo Oliveira <rodrigoliver.souza@gmail.com>
2025-06-03 09:32:02 -0400

    Atualiza a formatação da mensagem de localização na função normalizeWapiMessageV2025_1, adicionando um ícone de localização para melhorar a apresentação dos dados de latitude e longitude nas mensagens recebidas.

--- src/controllers/channels/wapi/wapi-handlers-v2025_1.js
+++ src/controllers/channels/wapi/wapi-handlers-v2025_1.js
@@ -192,7 +192,7 @@ export async function normalizeWapiMessageV2025_1(webhookData, channel = null) {
     messageContent = webhookData.msgContent.listResponseMessage.title || '';
   } else if (webhookData.msgContent?.locationMessage) {
     messageType = 'location';
-    messageContent = `Localização: ${webhookData.msgContent.locationMessage.degreesLatitude}, ${webhookData.msgContent.locationMessage.degreesLongitude}`; //Salvar latitude e longitude
+    messageContent = `📍 ${webhookData.msgContent.locationMessage.degreesLatitude}, ${webhookData.msgContent.locationMessage.degreesLongitude}`; //Salvar latitude e longitude
     
     // Extrair dados de localização
     const locationData = webhookData.msgContent.locationMessage;
